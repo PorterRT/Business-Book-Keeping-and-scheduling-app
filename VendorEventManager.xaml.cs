8ee54c7dbec4f47dc4a20894d777a95f0546068b
Porter Runyon-Trapp <runyontrp@sou.edu>
2024-09-23 17:22:04 -0700

    built start vendorEventManager should outline everything on it before fully starting and add access to the page in the app.

---
+++ VendorEventManager.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vendor_App;
+
+public partial class VendorEventManager : ContentPage
+{
+    public VendorEventManager()
+    {
+        InitializeComponent();
+    }
+}
