4c34c4b6cde8a93f55737da634de59f798dd0a5c
Porter Runyon-Trapp <runyontrp@sou.edu>
2024-12-01 15:29:43 -0800

    hopeful fix

--- Platforms/MacCatalyst/CalendarService.cs
+++ Platforms/MacCatalyst/CalendarService.cs
@@ -2,7 +2,6 @@ using Vendor_App;
 using EventKit;
 using Foundation;
 
-
 [assembly: Dependency(typeof(Vendor_App.Platforms.MacCatalyst.CalendarService))]
 
 namespace Vendor_App.Platforms.MacCatalyst
@@ -12,22 +11,24 @@ namespace Vendor_App.Platforms.MacCatalyst
         public async Task AddEventToCalendar(string title, DateTime startDate, DateTime endDate, DateTime setUp, DateTime startTime, string location)
         {
             var eventStore = new EKEventStore();
-        var (accessGranted, error) = await eventStore.RequestFullAccessToEventsAsync();
 
-            if (!accessGranted)
-        {
-            throw new Exception("Calendar access not granted.");
-        }
+            // Request calendar access
+            var accessGranted = await eventStore.RequestAccessAsync(EKEntityType.Event);
+            if (!accessGranted.Item1)
+            {
+                throw new Exception("Calendar access not granted.");
+            }
 
-
+            // Create a new event
             var newEvent = EKEvent.FromStore(eventStore);
             newEvent.Title = title;
             newEvent.Notes = $"Setup Time: {setUp.ToShortTimeString()}\nStart Time: {startTime.ToShortTimeString()}";
             newEvent.Location = location;
-            newEvent.StartDate = (NSDate)startDate;
-            newEvent.EndDate = (NSDate)endDate;
+            newEvent.StartDate = ToNSDate(startDate); // Convert DateTime to NSDate
+            newEvent.EndDate = ToNSDate(endDate);     // Convert DateTime to NSDate
             newEvent.Calendar = eventStore.DefaultCalendarForNewEvents;
 
+            // Save the event
             NSError saveError;
             eventStore.SaveEvent(newEvent, EKSpan.ThisEvent, out saveError);
 
@@ -35,6 +36,14 @@ namespace Vendor_App.Platforms.MacCatalyst
             {
                 throw new Exception($"Failed to save event: {saveError.LocalizedDescription}");
             }
+        }
+
+        private NSDate ToNSDate(DateTime dateTime)
+        {
+            // Apple reference date: January 1, 2001
+            var referenceDate = new DateTime(2001, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var timeInterval = (dateTime.ToUniversalTime() - referenceDate).TotalSeconds;
+            return NSDate.FromTimeIntervalSinceReferenceDate(timeInterval);
         }
     }
 }
